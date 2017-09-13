01323878ba888c995dfe0b639b5c69caace61917
Magnus Jonsson <magnus@hpc2n.umu.se>
2017-09-13 11:30:11 +0200

    added canWrite and cleanup of everything in filter.py

--- puckodb/filter.py
+++ puckodb/filter.py
@@ -1,43 +1,80 @@
 import json
 
 class Filter():
-    
-
     def __init__(self,rules={}):
         self.rules = rules
 
     def setRules(self,rules):
         self.rules = rules
 
-    def filter(self,user,data):
+    def filter(self,users,show_data):
         allow = False
         result = {}
-        for k in data:
+        admin = False
+
+        for user in users:
+            if user in self.rules and '__admin__' in self.rules[user]:
+                admin = True
+
+        for key,data in show_data.items():
             read = False
             write = False
-            for u in ['__users__',user]:
-                if u in self.rules and k in self.rules[u]:
-                    if 'read' in self.rules[u][k] and self.rules[u][k]['read']:
-                        read = True
-                    if 'read' in self.rules[u][k] and not self.rules[u][k]['read']:
-                        read = False
-                    if 'write' in self.rules[u][k] and self.rules[u][k]['write']:
-                        write = True
-                    if 'write' in self.rules[u][k] and not self.rules[u][k]['write']:
-                        write = False
-                    if 'allowIf' in self.rules[u][k] and self.rules[u][k]['allowIf'] in data[k]:
-                        allow = True
-                    if 'denyIf' in self.rules[u][k] and self.rules[u][k]['allowIf'] in data[k]:
-                        allow = False
-            
-            if read or write:
-                result[k] = data[k]
+            for user in filter(lambda u: u in self.rules,users):
+                if not key in self.rules[user]:
+                    continue
+                rule = self.rules[user][key]
+                if 'read' in rule:
+                    read = rule['read']
+                if 'write' in rule:
+                    write = rule['write']
+                if 'allowIf' in rule and type(data) is list and rule['allowIf'] in data:
+                    allow = True
+                if 'allowIf' in rule and not type(data) is list and rule['allowIf'] == data:
+                    allow = True
+                if 'denyIf' in rule and type(data) is list and rule['denyIf'] in data:
+                    allow = False
+                if 'denyIf' in rule and not type(data) is list and rule['denyIf'] == data:
+                    allow = False
 
-        if not allow:
+            if read or admin:
+                result[key] = data
+
+        if not allow and not admin:
             return {} 
 
         return result
             
+    def canWrite(self,users,original_data,new_data):
+        allow = True
+        admin = False
+
+        for u in users:
+            if u in self.rules and '__admin__' in self.rules[u]:
+                admin = True
+
+        for key,data in original_data.items():
+            for user in filter(lambda u: u in self.rules,users):
+                if not key in self.rules[user]:
+                    continue
+                rule = self.rules[user][key]
+                if 'denyIf' in rule and rule['denyIf'] in data:
+                    allow = False
+
+        for key,data in new_data.items():
+            write = False
+            for user in filter(lambda u: u in self.rules,users):
+                if not key in self.rules[user]:
+                    continue
+                rule = self.rules[user][key]
+                if 'write' in rule and rule['write']:
+                    write = True
+                if 'denyIf' in rule and rule['denyIf'] in data:
+                    allow = False
+
+            if not write:
+                allow = False
+            
+        return admin or allow
 
 
 if __name__ == "__main__":
@@ -45,22 +82,51 @@ if __name__ == "__main__":
             '__users__': {
                 'hej': { 'read': True },
                 'hopp': { 'write': True },
-                'auth': { 'allowIf': 'public' },
+                'auth': { 'allowIf': '__users__' },
             },
             'magnus': {
+                '__admin__': True,
                 'auth': { 'allowIf': 'magnus' }
             }
         })
         
-    print json.dumps(f.filter('magnus',{
+    print json.dumps(f.canWrite(['__users__','magnus'],{
+            'hej':'hopp',
+            'gnu':'apa',
+            'hopp': '',
+            'auth': ['public','magnus']
+        },{
+            'hej':'groda'
+        }), indent= 4, sort_keys= True)
+        
+    print json.dumps(f.canWrite(['__users__'],{
+            'hej':'hopp',
+            'gnu':'apa',
+            'hopp': '',
+            'auth': ['public','magnus']
+        },{
+            'hej':'groda'
+        }), indent= 4, sort_keys= True)
+        
+    print json.dumps(f.canWrite(['__users__'],{
+            'hej':'hopp',
+            'gnu':'apa',
+            'hopp': '',
+            'auth': ['__users__','magnus']
+        },{
+            'hopp':'groda'
+        }), indent= 4, sort_keys= True)
+
+    print json.dumps(f.filter(['__users__','magnus'],{
         'hej':'hopp',
         'gnu':'apa',
         'hopp': '',
         'auth': ['public','magnus']
     }), indent= 4, sort_keys= True)
 
-    print json.dumps(f.filter('magnus',{
+    print json.dumps(f.filter(['__users__','magnu'],{
         'hej':'hopp',
         'gnu':'apa',
         'hopp': '',
+        'auth': ['__users__']
     }), indent= 4, sort_keys= True)
