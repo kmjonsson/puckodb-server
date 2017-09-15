76fee5fa679138bcac1d60a53bb7164cc8cc2566
Magnus Jonsson <fot@fot.nu>
2017-09-15 22:36:39 +0200

    Fixed traceback and added filter object.

--- server.py
+++ server.py
@@ -3,7 +3,10 @@ from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 from puckodb.storage import PuckoDbStorage
 from puckodb.puckodb import PuckoDb
 from puckodb.router import PuckoDbRouter
+from puckodb.filter import Filter
 import json
+
+import sys, traceback
 
 # Server Listener
 class PuckoWebSocketServer(SimpleWebSocketServer):
@@ -28,8 +31,32 @@ class PuckoWebSocketServer(SimpleWebSocketServer):
 
 # Server client instance
 class PuckoDbServer(WebSocket):
+    def error(self,message,extra={}):
+        r = {
+            'type': u'error',
+            'message': message
+        }
+        for k,v in extra.items():
+            r[k] = v
+        self.sendIt(json.dumps(r))
+
+    def ok(self,message,extra={}):
+        r = {
+            'type': u'ok',
+            'message': message
+        }
+        for k,v in extra.items():
+            r[k] = v
+        self.sendIt(json.dumps(r))
+
     def setUser(self,user):
-        self.user = user
+        self.users = [u'__public__']
+        if user and len(user):
+            self.users.extend([u'__users__',user])
+        print "setUser",self.address,self.users
+
+    def getUsers(self):
+        return self.users
 
     def sendAll(self,message):
         print(self.address, 'sendAll', unicode(message))
@@ -46,20 +73,41 @@ class PuckoDbServer(WebSocket):
         except Exception as inst:
             print type(inst)     # the exception instance
             print inst.args      # arguments stored in .args
-            print inst           
+            print inst
+            print '-'*60
+            traceback.print_exc(file=sys.stdout)
+            print '-'*60           
 
     def handleConnected(self):
        print(self.address, 'connected')
+       self.setUser('')
        self.server.addClient(self)
        self.server.router.parse(self,'{"type":"connected"}')
 
     def handleClose(self):
        self.server.removeClient(self)
-       self.server.router.parse(self,'{"type":"disconnected"}')
+       #self.server.router.parse(self,'{"type":"disconnected"}')
        print(self.address, 'closed')
 
 router = PuckoDbRouter()
+router.setFilter(Filter({
+            '__public__': {
+                '.tid': { 'read': True },
+                'foo': { 'read': True },
+                'auth': { 'allowIf': '__users__' },
+            },
+            '__users__': {
+                '.tid': { 'read': True },
+                'foo': { 'read': True },
+                'bar': { 'read': True },
+                'auth': { 'allowIf': '__users__', 'read': False },
+            },
+            'magnus': {
+                '__admin__': True
+            }
+}))
 router.setPuckoDb(PuckoDb(PuckoDbStorage('/tmp/pucko')))
 server = PuckoWebSocketServer('', 9999, PuckoDbServer)
+router.setServer(server)
 server.setRouter(router)
 server.serveforever()
