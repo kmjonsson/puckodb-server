b794e06493ad2c12b61eb534fa8d973427194e51
Magnus Jonsson <fot@fot.nu>
2017-09-15 22:36:17 +0200

    Lots of changes...

--- puckodb/router.py
+++ puckodb/router.py
@@ -5,64 +5,84 @@ class PuckoDbRouter():
     def setPuckoDb(self,puckodb):
         self.puckodb = puckodb
 
-    def setFilter(self,filter):
-        self.filter = filter
+    def setFilter(self,filterObj):
+        print "setFilter",filterObj
+        self.filterObj = filterObj
+
+    def setServer(self,server):
+        print "setServer",server
+        self.server = server
+
+    def sendUpdate(self,uuid):
+        o = self.puckodb.get(uuid)
+        print "sendUpdate (before filter)",uuid,o
+        print "sendUpdate (server)", self.server
+        for c in self.server.getClients():
+            d = self.filterObj.filterIt(c.getUsers(),o)
+            print "sendUpdate (after filter)",c.getUsers(),d
+            if '.tid' in d:
+                c.sendIt({uuid: d})
+
+    def sendObjs(self,client,objs):
+        for k,v in objs.items():
+            d = self.filterObj.filterIt(client.getUsers(),v)
+            if '.tid' in d:
+                client.sendIt({ k: d })
 
     def parse(self,client,message):
         print "Parse:",client.address,message
         incomming = json.loads(message)
         if not 'type' in incomming:
-            j = json.dumps({
-                'type': u'error',
-                'error': u"Missing type in incomming message"
-            })
-            client.sendIt(j)
+            client.error("no 'type' in message")
             return
+
         print "Type: " + incomming['type']
+
+        # AUTH
         if incomming['type'] == 'auth':
             client.setUser(incomming['user'])
-            j = json.dumps({
-                'type': u'ok',
-            })
-            client.sendIt(j)
+            client.ok('Login successful')
             return
+
+        if incomming['type'] == 'connected':
+            client.ok('Connected')
+            return
+
+        # Replay
+        if incomming['type'] == 'replay':
+            fromTid = incomming['from']
+            client.ok('replay successful')
+            objs = self.puckodb.getFrom(fromTid)
+            self.sendObjs(client,objs)
+            return
+
+        # CREATE
         if incomming['type'] == 'create':
-            id = self.puckodb.create()
-            self.puckodb.update(id,incomming)
-            print "Created: ",id
-            j = json.dumps({
-                'type': u'ok',
-                'id': id
-            })
-            client.sendIt(j)
+            uuid = self.puckodb.create()
+            self.puckodb.update(uuid,incomming)
+            client.ok('created',{ 'uuid': uuid })
+            self.sendUpdate(uuid)
             return
 
-        if incomming['type'] == 'update':
-            id = incomming['uuid']
-            self.puckodb.update(id,incomming)
-            print "updated: ",id
-            j = json.dumps({
-                'type': u'ok',
-                'id': id
-            })
-            client.sendIt(j)
-            o = self.puckodb.get(id)
-            client.sendAll(o)
+        if not 'uuid' in incomming:
+            client.error("no 'uuid' in message")
             return
 
+        # get UUID from message
+        uuid = incomming['uuid']
+
+        # UPDATE
+        if incomming['type'] == 'update':    
+            self.puckodb.update(uuid,incomming)
+            client.ok('updated',{'uuid':uuid})
+            self.sendUpdate(uuid)
+            return
+
+        # DELETE
         if incomming['type'] == 'delete':
-            id = incomming['uuid']
-            self.puckodb.delete(id)
-            print "deleted: ",id
-            j = json.dumps({
-                'type': u'ok',
-                'id': id
-            })
-            client.sendIt(j)
+            self.puckodb.delete(uuid)
+            client.ok('deleted',{'uuid':uuid})
+            self.sendUpdate(uuid)
             return
 
-        client.sendMessage(json.dumps({
-            'type': 'error',
-            'error': "Invalid type in incomming message"
-        }))
-
+        client.error("Invalid 'type' in message")
